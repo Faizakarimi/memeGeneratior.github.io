dc0da7e9477d7269f6ae87c162b86b51630b24da
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-08-08 13:59:40 +0500

    Create meme generator codes

---
+++ MemeGenerator/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Header() {
+    return (
+        <header className="header ">
+
+            <img src="../public/Troll Face.PNG" className="header--img"></img>
+            <h1 className="header--title">Meme Generator</h1>
+
+
+            <h4 className="header--project">React Course - Project 3</h4>
+
+
+        </header>
+
+    )
+}
